18205bd9aab6d916eb6c87c9c48ac7593ee2f0d6
AcevedoAdrian <acevedohugoadrian@gmail.com>
2023-06-21 12:40:59 -0300

    Add control the register,  tockens and mail config

--- controllers/usuario.controller.js
+++ controllers/usuario.controller.js
@@ -1,4 +1,5 @@
 import { check, validationResult } from "express-validator";
+import { generarId } from "../helpers/tockens.js";
 import Usuario from "../models/Usuario.js";
 
 const formularioLogin = (req, res) => {
@@ -15,29 +16,67 @@ const registrar = async (req, res) => {
     .notEmpty()
     .withMessage("El nombre no puede ir vacio")
     .run(req);
-  await check("email").isEmail().withMessage("Eso no parece un email").run(req);
+  await check("email")
+    .isEmail()
+    .withMessage("El email ingresado no es valido")
+    .run(req);
   await check("password")
     .isLength({ min: 6 })
     .withMessage("El Password deve ser de al menos 6 caracteres")
     .run(req);
-  await check("repetir_password")
-    .equals("password")
+  await check("repite_password")
+    .equals(req.body.password)
     .withMessage("Los Passwords no son iguales")
     .run(req);
+
   let resultadoValidation = validationResult(req);
+  // res.json(resultadoValidation);
 
   // Cerificamos que el resultado este vacio
   if (!resultadoValidation.isEmpty()) {
     // Errores
     return res.render("auth/registro", {
       pagina: "Crear Cuenta",
+      // convertimos los error que resivimos desde express-validator y lo convertimos en array
       errores: resultadoValidation.array(),
+      usuario: {
+        nombre: req.body.nombre,
+        correo: req.body.email,
+      },
     });
   }
-  res.json(resultadoValidation.array());
 
-  const usuario = await Usuario.create(req.body);
-  res.json(usuario);
+  const existeUsuario = await Usuario.findOne({
+    where: { email: req.body.email },
+  });
+  if (existeUsuario) {
+    return res.render("auth/registro", {
+      pagina: "Crear Cuenta",
+      // convertimos los error que resivimos desde express-validator y lo convertimos en array
+      errores: [{ msg: "El usuario ya esta Registrado" }],
+      usuario: {
+        nombre: req.body.nombre,
+        correo: req.body.email,
+      },
+    });
+  }
+  const { nombre, email, password } = req.body;
+  // res.json(resultadoValidation.array());
+  // const usuario = await Usuario.create(req.body);
+  // res.json(usuario);
+  // Alamcenar un usuario
+  await Usuario.create({
+    nombre,
+    email,
+    password,
+    // mandar por email para validar que es una persona para confirmar su cuenta
+    token: generarId(),
+  });
+
+  res.render("templates/mensaje", {
+    pagina: "Cuenta creada correctamente",
+    mensaje: "Hemos enviados un Email de confirmacion. Presiona en el enlace.",
+  });
 };
 
 const formularioRecuperarPassword = (req, res) => {
--- models/Usuario.js
+++ models/Usuario.js
@@ -1,21 +1,37 @@
 import { DataTypes } from "sequelize";
+import bcrypt from "bcrypt";
 import db from "../config/db.js";
 
-const Usuario = db.define("usuarios", {
-  nombre: {
-    type: DataTypes.STRING,
-    allowNull: false,
+const Usuario = db.define(
+  "usuarios",
+  {
+    nombre: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    token: DataTypes.STRING,
+    comfirmado: DataTypes.BOOLEAN,
   },
-  email: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  password: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  token: DataTypes.STRING,
-  comfirmado: DataTypes.BOOLEAN,
-});
+  {
+    // Hook de squelize
+    hooks: {
+      // Metodo que actua antes de guardar en base de datos y recibe por parametros req.body = usuario
+      beforeCreate: async function (usuario) {
+        // genera la semilla o salt para despues usarla para hashear, mientras mas alto el numero mas dificil de desencriptar pero esta es tambien mas recursos
+        const salt = await bcrypt.genSalt(10);
+        // rescribe el valor del password hasheando el valor que tenia mas el salt, dando como resultado una contrasena encriptada
+        usuario.password = await bcrypt.hash(usuario.password, salt);
+      },
+    },
+  }
+);
 
 export default Usuario;
